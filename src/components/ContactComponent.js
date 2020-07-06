3056052e6bf325c136c8bab4de0835ae9b722e36
Mehrab Zaman <mehrab.zaman@outlook.com>
2020-07-07 03:31:03 +0600

    Assignment 3

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -166,7 +166,7 @@ class Contact extends Component {
                     </Col>
                   </Row>
                   <Row className="form-group">
-                    <Label htmlFor="feedback" md={2}>Your Feedback</Label>
+                    <Label htmlFor="message" md={2}>Your Feedback</Label>
                     <Col md={10}>
                       <Control.textarea model=".message" id="message" name="message" rows="12"
                       className="form-control" />
